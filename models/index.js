ee1e0929113e46b36d414fa1275f52d2c4ee109b
Heggyhere <heggyy@gmail.com>
2019-02-16 14:44:57 -0800

    application knows what port to run on, and what database to connect to - you're almost all set up to work in "production" on Heroku's servers!

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@
 
     const db = mongoose.connection;
 
+    // Update your database connection to point to Heroku's database. mongoose.connect method
     mongoose.connect(process.env.MONGODB_URI  || 'mongodb://localhost/jedi-db', { useNewUrlParser: true });
 
     db.once('open', function() {
