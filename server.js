296a42e19d599a06bc343d8b0879688f1e422410
salay <siri.alay1@gmail.com>
2019-02-15 16:11:47 -0800

    can you see the new folders?

---
+++ server.js
@@ -0,0 +1,29 @@
+// require express and other modules
+const express = require('express');
+const app = express();
+
+// parse incoming urlencoded form data
+// and populate the req.body object
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// allow cross origin requests (optional)
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/************
+ * DATABASE *
+ ************/
+
+const db = require('./models');
+
+/**********
+ * ROUTES *
+ **********/
+
+// Serve static files from the `/public` directory:
+// i.e. `/images`, `/scripts`, `/styles`
