6b6cfdb49b0cbaf40e3322f67c966cefea543ef7
chike onuorah <chikeonuorah@chike-onuorahs-MacBook-Pro.local>
2019-02-16 13:11:35 -0800

    fix typo in seed file

--- seed.js
+++ seed.js
@@ -39,7 +39,7 @@ for(let i = 0; i <  apprenticeships.length; i++) {
 
                     if (err) {
 
-                        return onsole.log(err);
+                        return console.log(err);
 
                     }
                 });
