e539fd0aa065292edb25b4387e711f7c619f9195
Heggyhere <heggyy@gmail.com>
2019-02-16 23:00:02 -0800

    appreData show on index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,12 @@ console.log("Sanity Check: JS is working!");
 
 $( document ).ready(function() {
   $("input").change(function(){
-    alert("The text has been changed.");
+    // alert("The text has been changed.");
+    // call my api
+    $.ajax({
+      url: "./api/apprenticeships"
+    }).then(function(apprenData) {
+      console.log(apprenData);
+    });
   });
 });
