e42af86cd5c83b92f8d63e633aa041f5f22569fd
salay <siri.alay1@gmail.com>
2019-02-19 16:28:44 -0800

    resolved merge conflict issues?

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const db = require('./models');
 
 // Serve static files from the `/public` directory:
 // i.e. `/images`, `/scripts`, `/styles`
+
 app.use(express.static('public'));
 
 /*
@@ -38,160 +39,285 @@ app.get('/', function homepage(req, res) {
 });
 
 
+app.get('/add', function addPage(req, res) {
+    res.sendFile(__dirname + '/views/add.html');
+  });
+
+  app.get('/profile', function addPage(req, res) {
+    res.sendFile(__dirname + '/views/profile.html');
+  });
+
+
 /*
  * JSON API Endpoints
  */
 
 app.get('/api', (req, res) => {
-  // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
-  // It would be seriously overkill to save any of this to your database.
-  // But you should change almost every line of this response.
-  res.json({
+  // all api endpoints are in the json object below
 
-    woopsIForgotToDocumentAllMyEndpoints: false, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+  res.json({
+    appTitle: "HackerJedi!",
+    appContributors: "Chike, Siri, and Heggy",
+    message: "this documents all api endpoints!",
+    GitHubLink: "CHANGE THIS", // CHANGE ME
+    HerokuLink: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Do you really want to know more about me? (I'm flattered)"},
-      {method: "GET", path: "/api/selfcare", despcription: "Index of all the things I wish existed, the places I want to go, things I want to do, & things I want" },
-      {method: "POST", path: "/api/selfcare", description: "Create new selfcare item"},
-      {method: "PUT", path: "/api/selfcare/:id", description: "Edit a selfcare item and update it"},
-      {method: "DELETE", path: "/api/selfcare/:id", description: "Delete a selfcare item"}
-      //extra: check off a selfcare item
-      //extra extra: search for a selfcare item
+      {method: "GET", path: "/profile", despcription: "View your user profile here" },
+      {method: "GET", path: "/api/add", despcription: "Add a new opportunity here. View, edit, & delete the one's you've added" },
+      {method: "POST", path: "/api/add", description: "Create new apprenticeship/opportunity"},
+      {method: "PUT", path: "/api/add/:id", description: "Edit an apprenticeship and update it"},
+      {method: "DELETE", path: "/api/add/:id", description: "Delete an apprenticeship that's no longer available/relevant"},
+      {method: "GET", path: "/api/opportunities", despcription: "View all opportunities for breaking into tech here!"}
+      //for later: search through opportunities
+      //maybe we can add algolia.com's search bar plugin
+      //or create our own search queries.
     ]
   })
 });
 
-app.get('/api/profile', (req, res) => {
-  res.json({
-    name: "Siri",
-    githubUsername: "salay",
-    githubLink: "http://www.github.com",
-    githubProfileImage: "really",
-    personalSiteLink: "deploymentInProgress",
-    currentCity: "San Francisco",
-    previouslyLived: [
-      {address: "0n The Bayou", cityName: "New Orleans", state: "Louisiana", country: "U.S."},
-      {address: "21 Jump St.", cityName: "Vancouver", state: "British Columbia", country: "Canada"},
-      {address: "12 Grimmauld Pl.", cityName: "London", state: "n/a", country: "England"}
-    ]
-  });
-});
-
-app.get('/api/selfcare', (req, res) => {
-  db.SelfCare.find({}, function (err, selfCareItem) {
-    if (err) {
-      console.log("error: " + err);
-    }
-    res.json(selfCareItem);
-  });
-})
-
-
-app.post('/api/selfcare', (req, res) => {
-  //create new selfcare item
-  let newSelfCare = new db.SelfCare({
-    //this gets from the form on index.html ... how to get from a form on another page?
-    task: req.body.task,
-    description: req.body.description,
-    byWhen: req.body.byWhen,
-    dateCompleted: req.body.dateCompleted
-  });
-    newSelfCare.save(function(err, book){
+app.get('/user', (req, res) => {
+    db.User.find({}, function (err, user) {
         if (err) {
           console.log("error: " + err);
         }
-        console.log("created ", newSelfCare);
-        res.json(newSelfCare);
+        res.json(user);
+      });
+      //want to display user info here after user logs in
+})
+
+
+app.delete('/user/:id', function (req, res) {
+    console.log('deleted user ID is ', req.params);
+   db.User.findOneAndDelete
+    ( {_id: req.params.id},
+    (err, deletedUser) => {
+      if (err) {
+        console.log("the error is " + err);
+      }
+       res.json(deletedUser);
+     });
+  })
+
+
+
+
+
+app.get('/api/add', (req, res) => {
+    db.Apprenticeship.find({}, function (err, apprenticeship) {
+        if (err) {
+          console.log("error: " + err);
+        }
+        res.json(apprenticeship);
       });
 })
 
 
-// app.post('api/selfcare', function(req, res){
-// let newTask = req.body;
-//  newTask.create(newTask, (err, savedTask) => {
-//     if(err) { return console.log(err) }
-//     res.json(savedTodo);
-
-// })
-
-
-
-
+// app.post('/api/add', (req, res) => {
+//     db.Apprenticeship(req.body).save(function(err, apprenticeship){
+//           if (err) {
+//             console.log("error: " + err);
+//           }
+//           console.log("created " + apprenticeship);
+//           res.json(apprenticeship);
+//         });
+//   })
 
 
+//   app.post('/profile/add', (req, res) => {
+//     //create new selfcare item
+//     let user = new db.User({
+//       //this gets from the form on index.html ... how to get from a form on another page?
+//       firstName: req.body.firstName,
+//       city: req.body.city,
+//       url: req.body.url,
+//       description: req.body.description,
+//    //userCreated?
+//     });
+//       newAppr.save(function(err, apprenticeship){
+//           if (err) {
+//             console.log("error: " + err);
+//           }
+//           console.log("created " + newAppr);
+//           res.json(newAppr);
+//         });
+//   })
 
-app.put('/api/selfcare/:id', function(req,res){
-    console.log('updated selfcare list: ', req.params);
-    const id = req.params.id;
-    db.SelfCare.findOneAndUpdate(
-      { _id: id},
-      req.body,
-      {new: true},
-      (err, updatedSelfCare) => {
+
+
+//     let  = {
+//       name: req.body.name,
+//       email: req.body.email,
+//       user_created: ''
+//     }
+
+
+
+
+//Chike's
+
+// app.post('/api/add', (req, res ) => {
+
+//      let newUser = new db.User({
+//           //this gets from the form on index.html ... how to get from a form on another page?
+//           firstName: req.body.firstName,
+//           lastName: req.body.lastName,
+//            email: req.body.email,
+//           company: req.body.company,
+//        //userCreated?
+//         });
+
+//     User.create(newUser, (err, userCreated)=> {
+//       if (err) {
+//         return console.log(err);
+//       }
+
+//      userCreated.save((err, newUser)=>{
+
+//         if (err){
+
+//           return console.log(err);
+
+//          }
+
+//         let apprenticeship = {
+//           company: req.body.company,
+//           url: req.body.url,
+//           description: req.body.description,
+//           city: req.body.city
+//          }
+
+//         apprenticeship['user_created'] = userCreated._id;
+
+//         Apprenticeship.create(apprenticeship, (err, newAppr) => {
+
+//           newAppr.save((err)=>{
+
+//             if (err) {
+
+//                return console.log(err);
+
+//             } else {
+//               res.json(newAppr);
+//             }
+//           });
+//         });
+//      });
+//     });
+//   });
+
+
+//Heggy's
+// Creat new apprenticeship
+app.post('/api/add', (req, res) => {
+    // create new apprenticeship with form data ('req.body')
+    console.log("body is ", req.body);
+    let newApprenticeship = new db.Apprenticeship({
+      company: req.body.company,
+      city: req.body.city,
+      url: req.body.url,
+      description: req.body.description,
+      user_created: null,
+    });
+    // find the user from req.body
+    db.User.findOne({email: req.body.email}, (err, user) => {
+      if (err) {
+        return console.log(err);
+      }
+      // if that user doesn't exist yet, create a new one
+      if (user === null) {
+        db.User.create({name: req.body.user, email: req.body.email}, (err, newUser) => {
+          if (err) {
+            return console.log(err);
+          }
+          createApprenticeshipWithUser(newApprenticeship, newUser, res);
+        });
+      } else {
+        createApprenticeshipWithUser(newApprenticeship, user, res);
+      }
+    });
+    // save new Apprentice
+  });
+
+  // create Apprenticeship With new User function
+  function createApprenticeshipWithUser(apprenticeship, user, res) {
+    // add this user to the apprenticeship
+    apprenticeship.user_created = user;
+    // save new apprenticeship to database
+    apprenticeship.save(function(err, book) {
+      if (err) {
+        return console.log("save error: " + err)
+      }
+      console.log("saved ", apprenticeship.company);
+      // send back the apprenticeship
+      res.json(apprenticeship);
+    });
+  }
+
+
+
+
+
+app.put('/api/add/:id', function(req,res){
+    // ^^^ get the id of the apprenticeship
+    //need to also get the user id from the frontend (login)
+    console.log('updated apprenticeships: ', req.params);
+    db.Apprenticeship.findOneAndUpdate({ _id: req.params.id},req.body,{new: true})
+      .populate('user_created')
+      .exec( (err, apprenticeship) => {
       if (err) {
         console.log("the error is " + err);
       }
-      res.json(updatedSelfCare);
+    //    apprenticeship.user_created.email = req.body.email;
+    //    apprenticeship.company = requ.body.company;
+    //    apprenticeship.url = requ.body.url;
+    //    apprenticeship.city = req.body.city;
+    //    apprenticeship.description = req.body.description;
+      res.json(apprenticeship);
     });
   });
 
 
 
 
-app.delete('/api/selfcare/:id', function (req, res) {
-  console.log('deleted selfcare is ', req.params);
-  let id = req.params.id;
- db.SelfCare.findOneAndDelete
-  ( {_id: id},
-  (err, deletedItem) => {
-    if (err) {
-      console.log("the error is " + err);
-    }
-     res.json(deletedItem);
-   });
-})
+//   app.put('/api/add/:id', function(req,res){
+//     console.log('updated apprenticeships: ', req.params);
 
+//     db.Apprenticeship.findOneAndUpdate({ _id: req.params.id})
 
+//     .populate('user_created')
 
+//     .exec(err, apprenticeship) => {
+//         if (err) {
+//             console.log("the error is " + err);
+//           }
+//        apprenticeship.user_created.email = req.body.email;
+//        apprenticeship.company = requ.body.company;
+//        apprenticeship.url = requ.body.url;
+//        apprenticeship.city = req.body.city;
+//        apprenticeship.description = req.body.description;
 
-// db.selfCare.create(newWish, (err, savedWish) => {
-//   if(err) { return console.log(err) }
-//   res.json(savedWish);
-// })
-// newBook.save(function(err, book){
-//   if (err) {
-//     console.log("create error: " + err);
-//   }
-//   console.log("created ", book.title);
-//   res.json(book);
-// });
-
-// let newTodo = req.body;
-
-// // create new todo in db
-// newTodo.create(newTodo, (err, savedTodo) => {
-//     if(err) { return console.log(err) }
-//     res.json(savedTodo);
-// });
-// });
-
-// app.post('/api/books', (req, res) => {
-//   // create new book with form data (`req.body`)
-//   let newBook = new db.Book({
-//     title: req.body.title,
-//     image: req.body.image,
-//     releaseDate: req.body.releaseDate,
+//        res.json(apprenticeship);
+//     }
 //   });
 
-/**********
- * SERVER *
- **********/
 
-// listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server is up and running on http://localhost:3000/');
+
+app.delete('/api/add/:id', function (req, res) {
+    console.log('deleted apprenticeship is ', req.params);
+   db.Apprenticeship.findOneAndDelete
+    ( {_id: req.params.id},
+    (err, deletedApprenticeship) => {
+      if (err) {
+        console.log("the error is " + err);
+      }
+       res.json(deletedApprenticeship);
+     });
+  })
+
+
+
+
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log('listening on port 3000');
 });
