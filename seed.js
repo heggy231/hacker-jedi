6d31e69b88f2d61c75b09c75ad3186a54664b0c3
salay <siri.alay1@gmail.com>
2019-02-19 16:49:07 -0800

    resolved a few conflicts manually

--- seed.js
+++ seed.js
@@ -5,106 +5,106 @@ const User = require('./models/user');
 let apprenticeship_list = [
   {
     company: "Pinterest",
-    link: "https://careers.pinterest.com/careers/",
-    location: "San Francisco, CA",
+    url: "https://careers.pinterest.com/careers/",
+    city: "San Francisco, CA",
     description: "Pinterest's apprentice program focuses on hiring engineers from non-traditional backgrounds who are comfortable with basic programming principles.",
     user_created: "siri.alay1@gmail.com",
   },
   {
     company: "IFTTT",
-    link: "https://ifttt.com/apprenticeship",
-    location: "San Francisco, CA",
+    url: "https://ifttt.com/apprenticeship",
+    city: "San Francisco, CA",
     description: 'If This Then That, also known as IFTTT, is a free web-based service to connect all your apps and devices into one "applet".',
     user_created: "siri.alay1@gmail.com",
   },
   {
     company: "DevMynd",
-    link: "https://www.devmynd.com/culture/careers/",
-    location: ["Chicago, IL", "San Francisco, CA"],
+    url: "https://www.devmynd.com/culture/careers/",
+    city: ["Chicago, IL", "San Francisco, CA"],
     description: "DevMynd is a software development and user experience consultancy with a passion for crafting quality solutions.",
     user_created: "siri.alay1@gmail.com",
   },
   {
     company: "Dropbox IGNITE",
-    link: "https://www.dropbox.com/jobs/all-jobs",
-    location: "San Francisco, CA",
+    url: "https://www.dropbox.com/jobs/all-jobs",
+    city: "San Francisco, CA",
     description: "Dropbox IGNITE is an apprenticeship program geared towards professionals with non-traditional software engineering backgrounds who are looking to start or re-start their professional career.",
     user_created: "heggyy@gmail.com",
   },
   {
     company: "Adobe Digital Academy",
-    link: "http://www.adobe.com/corporate-responsibility/education/digital-academy.html",
-    location: ["San Francisco, CA", "Salt Lake City, UT"],
+    url: "http://www.adobe.com/corporate-responsibility/education/digital-academy.html",
+    city: ["San Francisco, CA", "Salt Lake City, UT"],
     description: "Adobe Digital Academy offers nontraditional candidates the education and, contingent upon technical performance, the experience they need to launch successful careers in web development.",
     user_created: "heggyy@gmail.com",
   },
   {
     company: "Techtonica",
-    link: "https://techtonica.org/",
-    location: "San Francisco, CA",
+    url: "https://techtonica.org/",
+    city: "San Francisco, CA",
     description: "Techtonica offers six-month tech apprenticeships with living stipends and laptops to women and non-binary adults with low incomes, then place graduates with sponsor companies for at least three months of full-time work.",
     user_created: "heggyy@gmail.com",
   },
   {
     company: "Twilio Hatch",
-    link: "https://www.twilio.com/company/jobs",
-    location: "San Francisco, CA",
+    url: "https://www.twilio.com/company/jobs",
+    city: "San Francisco, CA",
     description: "The apprenticeship, known as Hatch, is a six-month program which aims to equip individuals having non-traditional technical backgrounds with industry experience in designing, developing, and delivering production-ready software systems.",
     user_created: "chike.onuorah@gmail.com",
   },
   {
     company: "Ada Developers Academy",
-    link: "https://www.adadevelopersacademy.org/",
-    location: "Seattle, WA",
+    url: "https://www.adadevelopersacademy.org/",
+    city: "Seattle, WA",
     description: "Ada Developers Academy is a training program for women and gender diverse people who want to become software developers.",
     user_created: "chike.onuorah@gmail.com",
   },
   {
     company: "Apprenti",
-    link: "https://apprenticareers.org/",
-    location: ["Seattle, WA", "Detroit, MI", "Bend, OR", "Eugene, OR", "Columbus, OH", "Herndon, VA"],
+    url: "https://apprenticareers.org/",
+    city: ["Seattle, WA", "Detroit, MI", "Bend, OR", "Eugene, OR", "Columbus, OH", "Herndon, VA"],
     description: "Apprenti provides a proven, reliable pipeline for underrepresented groups such as minorities, women, and veterans to gain training, certification, and placement within the talent-hungry tech industry.",
     user_created: "chike.onuorah@gmail.com",
   },
   {
     company: "LaunchCode",
-    link: "https://www.launchcode.org/",
-    location: "St. Louis, MO",
+    url: "https://www.launchcode.org/",
+    city: "St. Louis, MO",
     description: "LaunchCode is a non-profit organization that works with hundreds of companies to set up paid apprenticeships in technology for talented people who lack traditional credentials.",
     user_created: "chike.onuorah@gmail.com",
   },
   {
     company: "IQ Foundry",
-    link: "http://www.iqfoundry.com/careers",
-    location: "Madison, WI",
+    url: "http://www.iqfoundry.com/careers",
+    city: "Madison, WI",
     description: "IQ Foundry is a marketing agency that designs and develops digital media including custom apps and feature-rich websites.",
     user_created: "chike.onuorah@gmail.com",
   },
   {
     company: "Stack Overflow",
-    link: "https://boards.greenhouse.io/stackapprentice",
-    location: "New York, NY",
+    url: "https://boards.greenhouse.io/stackapprentice",
+    city: "New York, NY",
     description: "Stack Overflow's Web Developer Apprentice is an 11-week apprenticeship program where you'll work directly alongside a developer mentor on a real product team. Rather than doing independent 'intern' projects that may not ever ship, as an apprentice you'll work with your mentor to learn, review, debug, and ship features to real users. You'll also work collaboratively with other members of the team, including other developers, designers, and product managers.",
     user_created: "siri.alay1@gmail.com",
   },
   {
     company: "Spotify",
-    link: "https://ttp.nyc/spotifyfellowship/",
-    location: "New York, NY",
+    url: "https://ttp.nyc/spotifyfellowship/",
+    city: "New York, NY",
     description: "The Spotify NYC Technology Fellowship is aimed at helping engineers just starting their careers in tech and provides an opportunity to work within a team and build upon current skills.",
     user_created: "siri.alay1@gmail.com",
   },
   {
     company: "Intrepid Pursuits",
-    link: "http://info.intrepid.io/apprentice",
-    location: ["Cambridge, MA", "New York, NY"],
+    url: "http://info.intrepid.io/apprentice",
+    city: ["Cambridge, MA", "New York, NY"],
     description: "Intrepid’s Apprentice Program is an immersive, full-time 12-week training program that spans several disciplines: Project Management, Experience Design, iOS, Android and Web development.",
     user_created: "chike.onuorah@gmail.com",
   },
   {
     company: "Social Tables",
-    link: "https://socialtables.com",
-    location: "Washington, DC",
+    url: "https://socialtables.com",
+    city: "Washington, DC",
     description: "Social Tables is a global SaaS company offering a 3-month paid program targeting new developers without on the job work experience with the potential of a full-time offer to join the engineering team.",
     user_created: "chike.onuorah@gmail.com",
   },
@@ -135,7 +135,7 @@ for(let i = 0; i <  apprenticeship_list.length; i++) {
       // if user email on apprenticship is equal to user_list email
       if (apprenticeship.user_created === users_list[j].email) {
         user = users_list[j];
-        // connect them 
+        // connect them
       }
     }
     // check if user db entry already exist
@@ -157,7 +157,7 @@ for(let i = 0; i <  apprenticeship_list.length; i++) {
         if (err) {
           return console.log(err);
         }
-        // set ref id (remote key) for apprenticeship link to user
+        // set ref id (remote key) for apprenticeship url to user
         apprenticeship['user_created'] = userCreated._id;
         // add the db id to the user obj
         user["id"] = userCreated._id;
