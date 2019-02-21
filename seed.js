186f5c4606e03dac55f2830dd42147991f8f50d7
chike onuorah <chikeonuorah@chike-onuorahs-MacBook-Pro.local>
2019-02-21 12:19:25 -0800

    render seed files

--- seed.js
+++ seed.js
@@ -1,28 +1,124 @@
-
 const db = require('./models');
 const User = require('./models/user');
 const Apprenticeship = require('./models/apprenticeship');
 
 
-let description = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
-let contact = 'contact@contact.com';
-let url = 'www.apprenticeship.com';
+let apprenticeships = [
+    {
+      company: "Pinterest",
+      url: "https://careers.pinterest.com/careers/",
+      city: "San Francisco, CA",
+      description: "Pinterest's apprentice program focuses on hiring engineers from non-traditional backgrounds who are comfortable with basic programming principles.",
+    },
+    {
+      company: "IFTTT",
+      url: "https://ifttt.com/apprenticeship",
+      city: "San Francisco, CA",
+      description: 'If This Then That, also known as IFTTT, is a free web-based service to connect all your apps and devices into one "applet".',
+    },
+    {
+      company: "DevMynd",
+      url: "https://www.devmynd.com/culture/careers/",
+      city: ["Chicago, IL", "San Francisco, CA"],
+      description: "DevMynd is a software development and user experience consultancy with a passion for crafting quality solutions.",
+    },
+    {
+      company: "Dropbox IGNITE",
+      url: "https://www.dropbox.com/jobs/all-jobs",
+      city: "San Francisco, CA",
+      description: "Dropbox IGNITE is an apprenticeship program geared towards professionals with non-traditional software engineering backgrounds who are looking to start or re-start their professional career.",
+    },
+    {
+      company: "Adobe Digital Academy",
+      url: "http://www.adobe.com/corporate-responsibility/education/digital-academy.html",
+      city: ["San Francisco, CA", "Salt Lake City, UT"],
+      description: "Adobe Digital Academy offers nontraditional candidates the education and, contingent upon technical performance, the experience they need to launch successful careers in web development.",
+    },
+    {
+      company: "Techtonica",
+      url: "https://techtonica.org/",
+      city: "San Francisco, CA",
+      description: "Techtonica offers six-month tech apprenticeships with living stipends and laptops to women and non-binary adults with low incomes, then place graduates with sponsor companies for at least three months of full-time work.",
+    },
+    {
+      company: "Twilio Hatch",
+      url: "https://www.twilio.com/company/jobs",
+      city: "San Francisco, CA",
+      description: "The apprenticeship, known as Hatch, is a six-month program which aims to equip individuals having non-traditional technical backgrounds with industry experience in designing, developing, and delivering production-ready software systems.",
+    },
+    {
+      company: "Ada Developers Academy",
+      url: "https://www.adadevelopersacademy.org/",
+      city: "Seattle, WA",
+      description: "Ada Developers Academy is a training program for women and gender diverse people who want to become software developers.",
+    },
+    {
+      company: "Apprenti",
+      url: "https://apprenticareers.org/",
+      city: ["Seattle, WA", "Detroit, MI", "Bend, OR", "Eugene, OR", "Columbus, OH", "Herndon, VA"],
+      description: "Apprenti provides a proven, reliable pipeline for underrepresented groups such as minorities, women, and veterans to gain training, certification, and placement within the talent-hungry tech industry.",
+    },
+    {
+      company: "LaunchCode",
+      url: "https://www.launchcode.org/",
+      city: "St. Louis, MO",
+      description: "LaunchCode is a non-profit organization that works with hundreds of companies to set up paid apprenticeships in technology for talented people who lack traditional credentials.",
+    },
+    {
+      company: "IQ Foundry",
+      url: "http://www.iqfoundry.com/careers",
+      city: "Madison, WI",
+      description: "IQ Foundry is a marketing agency that designs and develops digital media including custom apps and feature-rich websites.",
+    },
+    {
+      company: "Stack Overflow",
+      url: "https://boards.greenhouse.io/stackapprentice",
+      city: "New York, NY",
+      description: "Stack Overflow's Web Developer Apprentice is an 11-week apprenticeship program where you'll work directly alongside a developer mentor on a real product team. Rather than doing independent 'intern' projects that may not ever ship, as an apprentice you'll work with your mentor to learn, review, debug, and ship features to real users. You'll also work collaboratively with other members of the team, including other developers, designers, and product managers.",
+    },
+    {
+      company: "Spotify",
+      url: "https://ttp.nyc/spotifyfellowship/",
+      city: "New York, NY",
+      description: "The Spotify NYC Technology Fellowship is aimed at helping engineers just starting their careers in tech and provides an opportunity to work within a team and build upon current skills.",
+    },
+    {
+      company: "Intrepid Pursuits",
+      url: "http://info.intrepid.io/apprentice",
+      city: ["Cambridge, MA", "New York, NY"],
+      description: "Intrepid’s Apprentice Program is an immersive, full-time 12-week training program that spans several disciplines: Project Management, Experience Design, iOS, Android and Web development.",
+    },
+    {
+      company: "Social Tables",
+      url: "https://socialtables.com",
+      city: "Washington, DC",
+      description: "Social Tables is a global SaaS company offering a 3-month paid program targeting new developers without on the job work experience with the potential of a full-time offer to join the engineering team.",
+    },
+  ];
 
-const apprenticeship1 =  {company: 'Atomic', city: 'san francisco', description: description, url: url};
+  let users_list = [
+    {
+      name: "Siri Alay",
+      email: "siri.alay1@gmail.com",
+    },
+    {
+      name: "Heggy Castaneda",
+      email: "heggyy@gmail.com",
+    },
+    {
+      name: "Chike Onuorah",
+      email: "chike.onuorah@gmail.com",
+    },
+  ];
 
-const apprenticeship2 =  {company: 'Clique', city: 'boston', description: description, url: url};
-
-const apprenticeship3 =  {company: 'Detroit Labs', city: 'miami', description: description, url: url};
-
-const apprenticeships =  [ apprenticeship1, apprenticeship2, apprenticeship3 ];
 
 // **** only run for loop when want to insert data
 
 // for(let i = 0; i <  apprenticeships.length; i++) {
+//     let randIndex = Math.floor(  Math.random()  * users_list.length );
 //     let apprenticeship = apprenticeships[i];
-//     let newUser = {name: 'chike' + i, 
-//                    email: 'contact@contact.com',
-//                    user_created: ''};
+//     let newUser = {name: users_list[randIndex].name, 
+//                    email: users_list[randIndex].email};
 
 //     User.create(newUser, (err, userCreated)=> {
 //          if (err) {
